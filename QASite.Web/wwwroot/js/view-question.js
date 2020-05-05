5d5aa71551fb0a4c01f627c5b788344f0ea254cd
Avrumi Friedman <a.friedman07@gmail.com>
2020-05-04 22:31:45 -0400

    Add project files.

---
+++ QASite.Web/wwwroot/js/view-question.js
@@ -0,0 +1,21 @@
+﻿$(() => {
+    $("#like-question").on('click', () => {
+        const questionId = $("#like-question").data('question-id');
+        $.post('/questions/addquestionlike', { questionId }, () => {
+            updateLikes();
+            //$("#like-question").prop('disabled', true);
+            $("#like-question").removeClass('glyphicon-heart-empty');
+            $("#like-question").addClass('glyphicon-heart');
+            $("#like-question").unbind('click');
+        });
+    });
+
+    const updateLikes = () => {
+        const questionId = $("#likes-count").data('question-id');
+        $.get('/questions/getlikes', { questionId }, result => {
+            $("#likes-count").text(result.likes);
+        });
+    };
+
+    setInterval(updateLikes, 1000);
+});
